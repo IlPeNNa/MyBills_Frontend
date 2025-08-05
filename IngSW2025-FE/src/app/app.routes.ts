67a72141b6a4abf80ec7373274237bc9f36c1c4e
Alex Pennini <alex.pennini@edu.unife.it>
2025-08-05 20:31:50 +0200

    Initial upload of MyBills Frontend project with authentication system and enhanced footer

--- IngSW2025-FE/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import {SampleEntitiesComponent} from './component/sample-entities/sample-entities.component';
-
-export const routes: Routes = [
-  { path: 'sample-entities', component: SampleEntitiesComponent }
-];
-
